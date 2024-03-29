f70f3990444cd555c7c1f519dd5cc52741ddf520
Ping Jhakkrit <143404462+pinglive287@users.noreply.github.com>
2024-03-29 15:09:03 +0700

    create game menu

---
+++ application/controllers/GameLearningThai_controller.php
@@ -0,0 +1,26 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class GameLearningThai_controller extends CI_Controller
+{
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->database();
+        $this->load->helper(array('form', 'url', 'text'));
+    }
+
+    public function index()
+    {
+
+        $this->data['view_file'] = 'Game_LearningThai/gamepage';
+        $this->load->view(THEMES, $this->data);
+    }
+
+    public function Thaigame($ID)
+    {
+        
+        $this->data['view_file'] = 'Game_LearningThai/thaigame';
+        $this->load->view(THEMES, $this->data);
+    }
+}
--- application/views/firstpage.php
+++ application/views/firstpage.php
@@ -11,20 +11,25 @@ $themes =  base_url();
 
 <div class="container">
     <div class="row mt-5 text-center">
-        <div class="col">
+        <div class="col-md-3">
             <a href="<?= site_url('Test_controller') ?>" class="text-decoration-none text-dark"><h3 class="fw-bold">คลังข้อสอบ</h3></a>
         </div>
-        <div class="col">
+        <div class="col-md-3">
             <a href="<?= site_url('Learning_media_controller') ?>" target="_blank" class="text-decoration-none text-dark"><h3 class="fw-bold">สะกดคำ</h3></a>
         </div>
-        <div class="col">
+        <div class="col-md-3">
             <a href="<?= site_url('Readcorrectly_controller') ?>" target="_blank" class="text-decoration-none text-dark"><h3 class="fw-bold">อ่านออก อ่านถูก</h3></a>
         </div>
-        <div class="col">
+        <div class="col-md-3">
             <a href="<?= site_url('Readfluently_controller') ?>" target="_blank" class="text-decoration-none text-dark"><h3 class="fw-bold">อ่านเร็ว อ่านคล่อง</h3></a>
         </div>
-        <div class="col">
-            <a href="<?= site_url('Game_controller') ?>" target="_blank" class="text-decoration-none text-dark"><h3 class="fw-bold">เกมรอบรู้ภาษาไทย</h3></a>
+    </div>
+    <div class="row mt-5 text-center">
+        <div class="col-md-3">
+            <a href="<?= site_url('GameLearningThai_controller') ?>" target="_blank" class="text-decoration-none text-dark"><h3 class="fw-bold">เกมรอบรู้ภาษาไทย</h3></a>
+        </div>
+        <div class="col-md-3">
+            <a href="<?= site_url('GamePuzzle_controller') ?>" target="_blank" class="text-decoration-none text-dark"><h3 class="fw-bold">เกมปริศนาทายคำ</h3></a>
         </div>
     </div>
 </div>
